6880392806c7bdc08d8e376453e72573794379b9
Will Fairclough <will@suitespot.io>
2023-04-29 13:21:33 -0400

    Cleanup code to be more idiomatic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use aws_sdk_secretsmanager::{config::Region, Client};
+use std::env;
 
 #[tokio::main]
 async fn main() {
@@ -10,19 +11,16 @@ async fn main() {
     };
     let aws_config = config_loader.load().await;
     let client = Client::new(&aws_config);
-
-    let secret_id = std::env::var("AWS_SECRET_ID").expect("AWS_SECRET_ID is not set");
-
-    let resp = client.get_secret_value().secret_id(secret_id).send().await.expect("request for secrets failed");
-
-    if resp.secret_binary().is_some() {
-        panic!("secret_binary is not supported");
-    }
-    let secret = resp.secret_string.expect("secret_string is not set");
-
-    let secret_json = serde_json::from_str::<serde_json::Value>(&secret)
-        .map(|v| v.as_object().ok_or("not an object").map(|o| o.clone()))
-        .unwrap_or_else(|_| Err("secret could not be parsed as json"));
+    
+    let secret_id = env::var("AWS_SECRET_ID").expect("AWS_SECRET_ID is not set");
+    let resp = client.get_secret_value().secret_id(secret_id).send().await.expect("failed to get secret value");
+    
+    let secret = match resp.secret_binary() {
+        Some(_) => panic!("secret_binary is not supported"),
+        None => resp.secret_string.expect("secret_string is not set"),
+    };
+    
+    let secret_json = serde_json::from_str::<serde_json::Map<String, serde_json::Value>>(&secret);
     match secret_json {
         Ok(secret_obj) => {
             for (key, value) in secret_obj {
